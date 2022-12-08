da891f3383851a0b7a8838abc0ac9bf57a3a4cc8
Khadija BOUZZITE <73465801+khadija-bouzzite@users.noreply.github.com>
2022-12-08 13:39:47 +0100

    Add files via upload

---
+++ Exam/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Examen
+from .models import professeur
+
+admin.site.register(Examen)
+admin.site.register(professeur)
+# Register your models here.
